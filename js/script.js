aa064eb291d270d9932f5089f09bc4975e3601b9
Dimanuga75 <103803856+Dimanuga75@users.noreply.github.com>
2022-04-28 12:48:13 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
+document.body.onload = function(){
+   setTimeout(function () {
+let preloader = document.getElementById ('preloud');
+if (!preloader.classList.contains('done'))
+{preloader.classList.add('done')}
+   }, 1000);
+}
+
 let btnmenu = document.querySelector('.header_burger')
 let menudisplay = document.querySelector('.header_menu')
-let btnclose = document.querySelector('._close')
+let menudisplay1 = document.querySelector('.header_menucont')
+let btnclose = document.querySelector('.header_close')
+let btncloses= document.querySelectorAll('.nav-link')
 
 btnmenu.onclick = function(e) {
       e.preventDefault()
        btnmenu.style.display = 'none';
-       menudisplay.style.display='block';
+       menudisplay1.classList.add("active");
+       menudisplay.classList.add("active");
+       menudisplay1.style.display = 'block';
+       menudisplay.style.display = 'block';
+             
 }
 btnclose.onclick = function(es) {
    es.preventDefault()
-   menudisplay.style.display='none';
    btnmenu.style.display = 'block';
-}   
-// $(document).ready(function() {
-//    $('.logo').on('click', function() {
-//       console.log('zdggd');
-//    })
-// })
+   menudisplay1.classList.remove("active");
+   menudisplay.classList.remove("active");
+   menudisplay1.style.display = 'none';
+   menudisplay.style.display = 'none'; 
+}  
+ for (const btn of btncloses) {
+   btn.addEventListener('click', function () {
+      btnmenu.style.display = 'block';
+      menudisplay1.classList.remove("active");
+      menudisplay.classList.remove("active");
+      menudisplay1.style.display = 'none';
+      menudisplay.style.display = 'none'; 
+   });
+ }
 
 $(window).scroll(function() {
    if ($(this).scrollTop() > 100 && $(this).scrollTop() < 1700) {
@@ -92,9 +113,20 @@ $(window).scroll(function() {
 //    $('.header').css('display', 'none');
    
   $( window ).width(function() { 
-  if ( $( this ).width() > 641){
+   if ( $( this ).width() > 991.78){
       $(window).scroll(function() {
-       if ($(this).scrollTop() > 200 && $(this).scrollTop() < 1700) {
+       if ($(this).scrollTop() > 200 && $(this).scrollTop() < 1200) {
+           $('.header_body ').css('display', 'flex');
+       }  
+    
+       else {
+           
+           $('.header_body ').css('display', 'none');
+       }
+   })
+  }  else if ( $( this ).width() > 641){
+      $(window).scroll(function() {
+       if ($(this).scrollTop() > 200 && $(this).scrollTop() < 1100) {
            $('.header_body ').css('display', 'flex');
        }  
     
@@ -114,7 +146,7 @@ $(window).scroll(function() {
                }) 
    } else {            
       $(window).scroll(function() {
-       if ($(this).scrollTop() > 200 && $(this).scrollTop() < 996) {
+       if ($(this).scrollTop() > 0 && $(this).scrollTop() < 996) {
            $('.header_body ').css('display', 'flex');
        }  
     
@@ -153,13 +185,121 @@ SmoothScroll({
    // Поддержка тачпада
    touchpadSupport   : true,
 })
+//Автослайдинг    
+//function auto_play() {  $(".btnright").click();};
+//var timemode = 10000;
+//var autoPlay = setInterval( auto_play, timemode );
+$( document ).ready(function() {
+$('#portfolio div[data-elem-type="button"]').addClass("sldcontbtn");
+ 
+$('.butsld , .sldelem').click(function(e){e.preventDefault();});
+var sumpic = $('.sldtitle').length-1;
+ 
+ var numsld = 0;
+ $(".btntab:not(:first)").addClass('tabmode');
+ $(".sldtitle:not(:first)").addClass('hide');
+ $(".sldtext1:not(:first)").addClass('hide');
+ $(".sldtext:not(:first)").addClass('hide');
+ $(".sldpic:not(:first)").addClass('hide');
+ $(".sldcontbtn:not(:first)").hide();
+ 
+ function sldelemhide(){
+   $(".sldtitle, .sldtext , .sldpic , .sldcontbtn, .sldtext1").addClass('hide');
+   $(".btntab").addClass('tabmode');
+};
+function sldelemshow(){
+   $(".sldtext1:eq("+numsld+"), .sldtitle:eq("+numsld+") , .sldtext:eq("+numsld+") , .sldpic:eq("+numsld+") , .sldcontbtn:eq("+numsld+")").removeClass('hide');
+   $(".sldnumber").html('0'+(numsld+1));
+   $(".btntab:eq("+numsld+")").removeClass('tabmode');
+};
+$(".btnright").click(function() {
+    sldelemhide();
+    if (numsld==sumpic){ numsld=0;sldelemshow();
+    }else{ numsld++;sldelemshow();
+    }; 
+});
+$(".btnleft").click(function() {
+    sldelemhide();
+    if (numsld==0){ numsld=sumpic;sldelemshow();
+    }else{ numsld--;sldelemshow(); 
+    }; 
+});
+//При клике на таб
+ $('.btntab').click(function(e) {e.preventDefault();
+    $('.btntab').addClass('tabmode');$(this).removeClass('tabmode'); 
+    numsld = $(".btntab").index(this); sldelemhide();sldelemshow();
+ });    
+ 
+//$(".butsld , .sldelem , .sldcontbtn ,.btntab").hover(
+//function(){  clearInterval(autoPlay);
+//},function(){  autoPlay = setInterval( auto_play, timemode );});   
+//});
+//document.addEventListener('DOMContentLoaded', function() {
+//    document.addEventListener('visibilitychange', function() {
+//        if (document.hidden) { clearInterval(autoPlay);
+//            } else { autoPlay = setInterval( auto_play, timemode );
+//            }
+//    });
+});
 
+const smoothLinks = document.querySelectorAll('.nav-link');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
 
+ let tagBody = document.getElementsByTagName('body');
+ document.addEventListener('DOMContentLoaded', () => {  
+ 
+   // Кнопка по которой происходит клик
+   let callBackButton = document.getElementById('callback-button');
+  
+   // Модальное окно, которое необходимо открыть
+   let modal1 = document.getElementById('modal-1');
+  
+   // Кнопка "закрыть" внутри модального окна
+   let closeButton = modal1.getElementsByClassName('modal__close-button')[0];
+  
+   // Тег body для запрета прокрутки
+   let tagBody = document.getElementById('body')
+  
+   callBackButton.onclick = function (e) {
+     e.preventDefault();
+     modal1.classList.add('modal_active');
+     tagBody.classList.add('hiden');
+   }
+  
+   closeButton.onclick = function (e) {
+     e.preventDefault();
+     modal1.classList.remove('modal_active');
+     tagBody.classList.remove('hiden');
+   }
+  
+   modal1.onmousedown = function (e) {
+     let target = e.target;
+     let modalContent = document.getElementsByClassName('modal_content')[0];
+     if (e.target.closest('.' + modalContent.className ) === null) {
+       this.classList.remove('modal_active')
+       tagBody.classList.remove('hiden');
+     }
+   };
+   let buttonOpenModal1 = document.getElementsByClassName('get-modal_1');
+ // Вызов модального окна несколькими кнопками на странице
+   for (let button of buttonOpenModal1) {
+     button.onclick = function (e) {
+       e.preventDefault();
+         modal1.classList.add('modal_active');
+         tagBody.classList.add('hiden');
+    
+      
+     }
+   }
+ });
 
-
-
-
-
-
-   
